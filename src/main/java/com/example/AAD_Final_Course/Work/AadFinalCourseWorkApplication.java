927d0d9ace3e224579d9f99ac281496ee3a3bf2d
veshon <vashonmabima12@gmail.com>
2024-10-27 16:44:21 +0530

    Entity Creation

--- src/main/java/com/example/AAD_Final_Course/Work/AadFinalCourseWorkApplication.java
+++ src/main/java/com/example/AAD_Final_Course/Work/AadFinalCourseWorkApplication.java
@@ -1,13 +1,25 @@
 package com.example.AAD_Final_Course.Work;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableMethodSecurity
+@EnableWebSecurity
 public class AadFinalCourseWorkApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AadFinalCourseWorkApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+
+		return new ModelMapper();
 	}
 
 }
