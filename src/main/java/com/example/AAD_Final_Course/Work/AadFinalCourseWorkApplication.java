77aeb747858f30f6c0a6accbf2fa4a38e0c402c0
veshon <vashonmabima12@gmail.com>
2024-10-27 16:15:43 +0530

    Initial Commit

---
+++ src/main/java/com/example/AAD_Final_Course/Work/AadFinalCourseWorkApplication.java
@@ -0,0 +1,13 @@
+package com.example.AAD_Final_Course.Work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AadFinalCourseWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AadFinalCourseWorkApplication.class, args);
+	}
+
+}
